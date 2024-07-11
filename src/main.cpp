c697f5ef86e68ae7f94ceaff53d52145ed9f58e4
Cameron Emery <75042648+mESmaC@users.noreply.github.com>
2024-07-11 16:33:30 -0600

    Clear vga buffer

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include <cstdint>
 
+void clear_screen() {
+    uint16_t* vga_buffer = (uint16_t*)0xB8000;
+    for (int i = 0; i < 80 * 25; i++) {
+        vga_buffer[i] = (uint16_t)' ' | (uint16_t)(0x0F << 8);
+    }
+}
+
 void write_char(char c, int x, int y) {
     uint16_t* vga_buffer = (uint16_t*)0xB8000;
     int offset = y * 80 + x;
@@ -13,6 +20,7 @@ void write_string(const char* str, int x, int y) {
 }
 
 extern "C" void main() {
+    clear_screen();
     write_string("Hello World!", 0, 0);
     while(1) {}
 }
